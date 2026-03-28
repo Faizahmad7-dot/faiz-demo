74873a27bc8c098da24ca38f43ba7538827d3cea
Faizahmad7-dot <faix8585@gmail.com>
2026-03-28 14:47:16 +0500

    added faix.cpp

---
+++ faix.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    string name;
+    cout << "Whats your name: " << endl;
+    cin >> name;
+    cout << "Hello World!" << endl;
+    return 0;
+}
